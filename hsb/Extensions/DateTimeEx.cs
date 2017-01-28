eeb009300ccdff874d2506208cbf270d1835b128
野村 修 <osamu_nomura@outlook.jp>
2017-01-28 14:43:32 +0900

    DateTimeExクラスにいくつかのメソッドを追加。

---
+++ hsb/Extensions/DateTimeEx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hsb.Extensions
+{
+    #region 【Static Class : DateTimeEx】
+    /// <summary>
+    /// DateTime 拡張メソッド
+    /// </summary>
+    public static class DateTimeEx
+    {
+        #region ■ Extension Methods
+
+        #region - SetYear : 対象日の年を変更した値を返す
+        /// <summary>
+        /// 対象日の年を変更した値を返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <param name="year">年</param>
+        /// <returns>指定した年に変更した日付</returns>
+        public static DateTime SetYear(this DateTime dt, int year)
+        {
+            return new DateTime(year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+        }
+        #endregion
+
+        #region - SetMonth : 対象日の月を変更した値を返す
+        /// <summary>
+        /// 対象日の月を変更した値を返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <param name="month">月</param>
+        /// <returns>指定した月に変更した日付</returns>
+        public static DateTime SetMonth(this DateTime dt, int month)
+        {
+            return new DateTime(dt.Year, month, dt.Day, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+        }
+        #endregion
+
+        #region - SetDay : 対象日の日を変更した値を返す
+        /// <summary>
+        /// 対象日の日を変更した値を返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <param name="day">日</param>
+        /// <returns>指定した日に変更した日付</returns>
+        public static DateTime SetDay(this DateTime dt, int day)
+        {
+            return new DateTime(dt.Year, dt.Month, day, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+        }
+        #endregion
+
+        #region - SafeSetYear : 対象日の年を変更した値を返す(安全版)
+        /// <summary>
+        /// 対象日の年を変更した値を返す
+        /// 値が不正な場合はNULLを返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <param name="year">年</param>
+        /// <returns>指定した年に変更した日付</returns>
+        public static DateTime? SafeSetYear(this DateTime dt, int year)
+        {
+            try { return dt.SetYear(year); }
+            catch (ArgumentOutOfRangeException) { return null;  }
+        }
+        #endregion
+
+        #region - SafeSetMonth : 対象日の月を変更した値を返す(安全版)
+        /// <summary>
+        /// 対象日の月を変更した値を返す
+        /// 値が不正な場合はNULLを返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <param name="month">月</param>
+        /// <returns>指定した月に変更した日付</returns>
+        public static DateTime? SafeSetMonth(this DateTime dt, int month)
+        {
+            try { return dt.SetMonth(month); }
+            catch (ArgumentOutOfRangeException) { return null; }
+        }
+        #endregion
+
+        #region - SafeSetDay : 対象日の日を変更した値を返す(安全版)
+        /// <summary>
+        /// 対象日の日を変更した値を返す
+        /// 値が不正な場合はNULLを返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <param name="day">日</param>
+        /// <returns>指定した日に変更した日付</returns>
+        public static DateTime? SafeSetDay(this DateTime dt, int day)
+        {
+            try { return dt.SetDay(day); }
+            catch (ArgumentOutOfRangeException) { return null; }
+        }
+        #endregion
+
+        #region - IsLeapYear : 対象日を含む年が閏年かどうかを返す
+        /// <summary>
+        /// 対象日を含む年が閏年かどうかを返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <returns>True : 閏年 / False: 閏年でない</returns>
+        public static bool IsLeapYear(this DateTime dt)
+        {
+            return DateTime.IsLeapYear(dt.Year);
+        }
+        #endregion
+
+        #region - BeginOfYear : 対象日を含む年の年初日を返す
+        /// <summary>
+        /// 対象日を含む年の年初日を返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <returns>対象日を含む年の年初日</returns>
+        public static DateTime BeginOfYear(this DateTime dt)
+        {
+            return new DateTime(dt.Year, 1, 1, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+        }
+        #endregion
+
+        #region - EndOfYear : 対象日を含む年の大みそかを返す
+        /// <summary>
+        /// 対象日を含む年の大みそかを返す
+        /// </summary>
+        /// <param name="dt">this 日付</param>
+        /// <returns>対象日を含む年の大みそか</returns>
+        public static DateTime EndOfYear(this DateTime dt)
+        {
+            return new DateTime(dt.Year, 12, 31, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+        }
+        #endregion
+
+        #region - BeginOfMonth : 対象日を含む月の月初日を返す
+        /// <summary>
+        /// 対象日を含む月の月初日を返す
+        /// </summary>
+        /// <param name="dt">this : 日付</param>
+        /// <returns>対象日を含む月の月初日</returns>
+        public static DateTime BeginOfMonth(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, 1, dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+        }
+        #endregion
+
+        #region - EndOfMonth : 対象日を含む月の月末日を返す
+        /// <summary>
+        /// 対象日を含む月の月末日を返す
+        /// </summary>
+        /// <param name="dt">this : 日付</param>
+        /// <returns>対象日付の月末日</returns>
+        public static DateTime EndOfMonth(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, DateTime.DaysInMonth(dt.Year, dt.Month), dt.Hour, dt.Minute, dt.Second, dt.Millisecond);
+        }
+        #endregion
+
+        #endregion
+    }
+    #endregion
+}
---
+++ hsbTests/Extensions/DateTimeExTests.cs
@@ -0,0 +1,236 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hsb.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hsb.Extensions.Tests
+{
+    #region 【Class : DateTimeExTests】
+    /// <summary>
+    /// DateTimeExのテストクラス
+    /// </summary>
+    [TestClass()]
+    public class DateTimeExTests
+    {
+        #region - SetYearTest
+        /// <summary>
+        /// Test for SetYear
+        /// </summary>
+        [TestMethod()]
+        public void SetYearTest()
+        {
+            DateTime dt, expected;
+
+            // case 1
+            dt = new DateTime(2017, 1, 28);
+            expected = new DateTime(2018, 1, 28);
+            Assert.AreEqual(expected, dt.SetYear(2018));
+
+            // case 2
+            dt = new DateTime(2016, 2, 29);
+            try
+            {
+                dt.SetYear(2018);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return;
+            }
+            Assert.Fail("例外が発生しなかった。");
+        }
+        #endregion
+
+        #region - SetMonthTest
+        /// <summary>
+        /// Test for SetMonth
+        /// </summary>
+        [TestMethod()]
+        public void SetMonthTest()
+        {
+            DateTime dt, expected;
+
+            // case 1
+            dt = new DateTime(2017, 1, 28);
+            expected = new DateTime(2017, 2, 28);
+            Assert.AreEqual(expected, dt.SetMonth(2));
+
+            // case 2
+            dt = new DateTime(2016, 1, 31);
+            try
+            {
+                dt.SetMonth(2);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return;
+            }
+            Assert.Fail("例外が発生しなかった。");
+        }
+        #endregion
+
+        #region - SetDayTest
+        /// <summary>
+        /// Test for SetDay
+        /// </summary>
+        [TestMethod()]
+        public void SetDayTest()
+        {
+            DateTime dt, expected;
+
+            // case 1
+            dt = new DateTime(2017, 1, 28);
+            expected = new DateTime(2017, 1, 10);
+            Assert.AreEqual(expected, dt.SetDay(10));
+
+            // case 2
+            dt = new DateTime(2016, 2, 10);
+            try
+            {
+                dt.SetDay(31);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return;
+            }
+            Assert.Fail("例外が発生しなかった。");
+        }
+        #endregion
+
+        #region - SafeSetYearTest
+        /// <summary>
+        /// Test for SafeSetYear
+        /// </summary>
+        [TestMethod()]
+        public void SafeSetYearTest()
+        {
+            DateTime dt;
+            DateTime? expected;
+
+            // case 1
+            dt = new DateTime(2017, 1, 28);
+            expected = new DateTime(2018, 1, 28);
+            Assert.AreEqual(expected, dt.SafeSetYear(2018));
+
+            // case 2
+            dt = new DateTime(2016, 2, 29);
+            Assert.AreEqual(null, dt.SafeSetYear(2018));
+        }
+        #endregion
+
+        #region - SafeSetMonthTest
+        /// <summary>
+        /// Test for SafeSetMonth
+        /// </summary>
+        [TestMethod()]
+        public void SafeSetMonthTest()
+        {
+            DateTime dt;
+            DateTime? expected;
+
+            // case 1
+            dt = new DateTime(2017, 1, 28);
+            expected = new DateTime(2017, 2, 28);
+            Assert.AreEqual(expected, dt.SafeSetMonth(2));
+
+            // case 2
+            dt = new DateTime(2016, 1, 31);
+            Assert.AreEqual(null, dt.SafeSetMonth(2));
+        }
+        #endregion
+
+        #region - SafeSetDayTest
+        /// <summary>
+        /// Test for SafeSetDay
+        /// </summary>
+        [TestMethod()]
+        public void SafeSetDayTest()
+        {
+            DateTime dt;
+            DateTime? expected;
+
+            // case 1
+            dt = new DateTime(2017, 1, 28);
+            expected = new DateTime(2017, 1, 31);
+            Assert.AreEqual(expected, dt.SafeSetDay(31));
+
+            // case 2
+            dt = new DateTime(2016, 2, 10);
+            Assert.AreEqual(null, dt.SafeSetDay(31));
+        }
+        #endregion
+
+        #region - IsLeapYearTest
+        /// <summary>
+        /// Test for IsLEapYear
+        /// </summary>
+        [TestMethod()]
+        public void IsLeapYearTest()
+        {
+            // Case 1
+            Assert.IsTrue(new DateTime(2016, 1, 1).IsLeapYear());
+            // Case 2
+            Assert.IsTrue(new DateTime(2000, 1, 1).IsLeapYear());
+            // Case 3
+            Assert.IsFalse(new DateTime(2100, 1, 1).IsLeapYear());
+            // Case 4
+            Assert.IsFalse(new DateTime(2017, 1, 1).IsLeapYear());
+        }
+        #endregion
+
+        #region - BeginOfYearTest
+        /// <summary>
+        /// Test for BeginOfYear
+        /// </summary>
+        [TestMethod()]
+        public void BeginOfYearTest()
+        {
+            var dt = new DateTime(2017, 1, 28);
+            var expected = new DateTime(2017, 1, 1);
+            Assert.AreEqual(expected, dt.BeginOfYear());
+        }
+        #endregion
+
+        #region - EndOfYearTest
+        /// <summary>
+        /// Test for EndOfYear
+        /// </summary>
+        [TestMethod()]
+        public void EndOfYearTest()
+        {
+            var dt = new DateTime(2017, 1, 28);
+            var expected = new DateTime(2017, 12, 31);
+            Assert.AreEqual(expected, dt.EndOfYear());
+        }
+        #endregion
+
+        #region - BeginOfMonthTest
+        /// <summary>
+        /// Test for BeginOfMonth
+        /// </summary>
+        [TestMethod()]
+        public void BeginOfMonthTest()
+        {
+            var dt = new DateTime(2017, 2, 28);
+            var expected = new DateTime(2017, 2, 1);
+            Assert.AreEqual(expected, dt.BeginOfMonth());
+        }
+        #endregion
+
+        #region - EndOfMonthTest
+        /// <summary>
+        /// Test for EndOfMonth
+        /// </summary>
+        [TestMethod()]
+        public void EndOfMonthTest()
+        {
+            var dt = new DateTime(2017, 1, 28);
+            var expected = new DateTime(2017, 1, 31);
+            Assert.AreEqual(expected, dt.EndOfMonth());
+        }
+        #endregion
+    }
+    #endregion
+}
