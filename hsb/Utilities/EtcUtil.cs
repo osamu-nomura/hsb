27721086abe43232ecad2d6cd66ac09169c58b37
野村 修 <osamu_nomura@outlook.jp>
2018-06-30 16:52:51 +0900

    EtcUtil.SafeExecute の関数版を追加

--- hsb/Utilities/EtcUtil.cs
+++ hsb/Utilities/EtcUtil.cs
@@ -55,6 +55,54 @@ namespace hsb.Utilities
         }
         #endregion
 
+        #region - SafeExecute : 例外を出さずに関数を実行する
+        /// <summary>
+        /// 例外を出さずに関数を実行する
+        /// </summary>
+        /// <typeparam name="T">型パラメータ</typeparam>
+        /// <param name="func">関数</param>
+        /// <returns>関数の戻り値と例外のタプル</returns>
+        public static (T Value, Exception E) SafeExecute<T>(Func<T> func)
+        {
+            try
+            {
+                if (func != null)
+                    return (func(), null);
+                return (default(T), null);
+            }
+            catch (Exception e)
+            {
+                return (default(T), e);
+            }
+        }
+        public static (T2 Value, Exception E) SafeExecute<T1,T2>(Func<T1,T2> func, T1 p)
+        {
+            try
+            {
+                if (func != null)
+                    return (func(p), null);
+                return (default(T2), null);
+            }
+            catch (Exception e)
+            {
+                return (default(T2), e);
+            }
+        }
+        public static (T3 Value, Exception E) SafeExecute<T1, T2, T3>(Func<T1, T2, T3> func, T1 p1, T2 p2)
+        {
+            try
+            {
+                if (func != null)
+                    return (func(p1, p2), null);
+                return (default(T3), null);
+            }
+            catch (Exception e)
+            {
+                return (default(T3), e);
+            }
+        }
+        #endregion
+
         #region - ValueIf : 値1を条件式で評価し、真の場合は値1でなければ値2を返す。
         /// <summary>
         /// 値1を条件式で評価し、真の場合は値1でなければ値2を返す。
--- hsbTests/Utilities/EtcUtilTests.cs
+++ hsbTests/Utilities/EtcUtilTests.cs
@@ -20,7 +20,23 @@ namespace hsbTests.Utilities.Tests
         {
             int n = 0;
             Assert.IsTrue(EtcUtil.SafeExecute(() => { n = (int)10.0; }) == null);
-            Assert.IsTrue(EtcUtil.SafeExecute(() => { n = n / 0; }) is Exception);
+            Assert.IsTrue(EtcUtil.SafeExecute(() => { n = n / 0; }) is DivideByZeroException);
+
+            var t1 = EtcUtil.SafeExecute(() => 10 / 10);
+            Assert.IsTrue(t1.Value == 1 && t1.E == null);
+            var t2 = EtcUtil.SafeExecute(() => n / 0);
+            Assert.IsTrue(t2.Value == 0 && t2.E is DivideByZeroException);
+
+            var t3 = EtcUtil.SafeExecute((p) => p / 10, 100);
+            Assert.IsTrue(t3.Value == 10 && t3.E == null);
+            var t4 = EtcUtil.SafeExecute((p) => p / 0, 100);
+            Assert.IsTrue(t4.Value == 0 && t4.E is DivideByZeroException);
+
+            var t5 = EtcUtil.SafeExecute((p1, p2) => p1 / p2, 100, 10);
+            Assert.IsTrue(t5.Value == 10 && t5.E == null);
+            var t6 = EtcUtil.SafeExecute((p1, p2) => p1 / p2, 100, 0);
+            Assert.IsTrue(t6.Value == 0 && t6.E is DivideByZeroException);
+
         }
         #endregion
 
