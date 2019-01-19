aae783679e045870d44003acc497e93dc028f7c9
野村 修 <osamu_nomura@outlook.jp>
2019-01-19 17:43:12 +0900

    Probabilityクラスの追加

---
+++ hsb/Classes/Probability.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hsb.Classes
+{
+    #region 【Class : Probability】
+    /// <summary>
+    /// 確率クラス
+    /// </summary>
+    public class Probability<T>
+    {
+        #region ■ Members
+
+        #region - _sum : 全確率の合計値
+        /// <summary>
+        /// 全確率の合計値
+        /// </summary>
+        private int _sum = 0;
+        #endregion
+
+        #region - _r : 乱数発生器
+        /// <summary>
+        /// 乱数発生器
+        /// </summary>
+        private Random _r = new Random();
+        #endregion
+
+        #endregion
+
+        #region ■ Properties
+
+        #region - Probabilities : 確率リスト
+        /// <summary>
+        /// 確率リスト
+        /// </summary>
+        public List<(T item, int ratio)> Probabilities { get; set; }
+        #endregion
+
+        #endregion
+
+        #region ■ Constructor
+
+        #region - Constructor(1)
+        /// <summary>
+        /// コンストラクタ(1)
+        /// </summary>
+        public Probability()
+        {
+        }
+        #endregion
+
+        #region - Constructor(2)
+        /// <summary>
+        /// コンストラクタ(2)
+        /// </summary>
+        /// <param name="probabilities">確率リスト</param>
+        public Probability(IEnumerable<(T item, int ratio)> probabilities) : this()
+        {
+            Probabilities = new List<(T item, int ratio)>(probabilities);
+            _sum = Probabilities.Sum(t => t.ratio);
+        }
+        #endregion
+
+        #endregion
+
+        #region ■ Methods
+
+        #region - Next : 次の値を取得する
+        /// <summary>
+        /// 次の値を取得する
+        /// </summary>
+        /// <returns>確率に応じた値</returns>
+        public T Next()
+        {
+            var n = _r.Next(_sum);
+            foreach(var t in Probabilities)
+            {
+                if (n < t.ratio)
+                    return t.item;
+                n -= t.ratio;
+            }
+            return Probabilities.FirstOrDefault().item;
+        }
+        #endregion
+
+        #endregion
+    }
+    #endregion
+}
---
+++ hsbTests/Classes/ProbabilityTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hsb.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using hsb.Extensions;
+
+namespace hsb.Classes.Tests
+{
+    #region 【Class : ProbabilityTests】
+    /// <summary>
+    /// Probabilityクラスのテスト
+    /// </summary>
+    [TestClass()]
+    public class ProbabilityTests
+    {
+        #region - NextTest
+        /// <summary>
+        /// Test of Next
+        /// </summary>
+        [TestMethod()]
+        public void NextTest()
+        {
+            var p = new Probability<string>(
+                new (string item, int ratio)[] {
+                    ("a", 50), ("b", 30), ("c", 20)
+                });
+            var d = new Dictionary<string, int>();
+            d.Add("a", 0);
+            d.Add("b", 0);
+            d.Add("c", 0);
+            Enumerable.Range(0, 1000).ForEach(n =>
+            {
+                var s = p.Next();
+                d[s] += 1;
+            });
+            Assert.IsTrue(true);
+        }
+        #endregion
+    }
+    #endregion
+}
