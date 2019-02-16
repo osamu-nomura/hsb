3cb01229791d34bd0996cd637defa225df1ff067
野村 修 <osamu_nomura@outlook.jp>
2019-02-16 21:02:41 +0900

    CalculatorUtil クラスの追加。（仕掛）

---
+++ hsb/Utilities/CalculatorUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hsb.Utilities
+{
+    #region 【Static Class : CalculatorUtil】
+    /// <summary>
+    /// 四則計算クラス
+    /// </summary>
+    public static class CalculatorUtil
+    {
+        #region ■ Static Methods
+
+        #region - Calculate : 数式を評価して計算を実行する
+
+        public static T Calculate<T>(string s)
+        {
+            return default(T);
+        }
+        #endregion
+
+        #endregion
+    }
+    #endregion
+}
