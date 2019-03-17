db447df86b3302f5d03f03858e7348a432df827f
野村 修 <nomura@tricogimmick.com>
2019-03-17 22:44:32 +0900

    SquareArrayColumnと、SquareArrayRowを、　ArrayExのインナークラスに変更。

--- hsb/Extensions/ArrayEx.cs
+++ hsb/Extensions/ArrayEx.cs
@@ -15,6 +15,174 @@ namespace hsb.Extensions
     /// </summary>
     public static class ArrayEx
     {
+        #region 【Class : SquareArrayColumn】
+        /// <summary>
+        /// 2次元配列向け列クラス
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public class SquareArrayColumn<T> : IEnumerable<T>
+        {
+            #region ■ Members
+            /// <summary>
+            /// 2次元配列
+            /// </summary>
+            private T[,] _array;
+            /// <summary>
+            /// 列インデックス
+            /// </summary>
+            private int _colIndex;
+            #endregion
+
+            #region ■ Properties
+
+            #region - Indexa : インデクサ
+            /// <summary>
+            /// インデクサ
+            /// </summary>
+            /// <param name="rowIndex">行インデックス</param>
+            /// <returns>要素</returns>
+            public T this[int rowIndex]
+            {
+                get => _array[rowIndex, _colIndex];
+                set => _array[rowIndex, _colIndex] = value;
+            }
+            #endregion
+
+            #region - Length : 長さ
+            /// <summary>
+            /// Length
+            /// </summary>
+            public int Length => _array.GetLength(0);
+            #endregion
+
+            #endregion
+
+            #region ■ Constructor
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="array">2次元配列</param>
+            /// <param name="rowIndex">行インデックス</param>
+            public SquareArrayColumn(T[,] array, int colIndex)
+            {
+                _array = array;
+                _colIndex = colIndex;
+            }
+            #endregion
+
+            #region ■ Methods
+
+            #region - GetEnumerator : GetEnumerator()の実装
+            /// <summary>
+            /// GetEnumerator()の実装
+            /// </summary>
+            /// <returns>IEnumerator<T></returns>
+            public IEnumerator<T> GetEnumerator()
+            {
+                for (var i = 0; i < Length; i++)
+                    yield return this[i];
+            }
+            #endregion
+
+            #region - GetEnumerator : GetEnumerator()の実装
+            /// <summary>
+            /// GetEnumerator()の実装
+            /// </summary>
+            /// <returns>IEnumerator</returns>
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
+            #endregion
+
+            #endregion
+        }
+        #endregion
+
+        #region 【Class : SquareArrayRow】
+        /// <summary>
+        /// 2次元配列向け行クラス
+        /// </summary>
+        /// <typeparam name="T">型パラメータ</typeparam>
+        public class SquareArrayRow<T> : IEnumerable<T>
+        {
+            #region ■ Members
+            /// <summary>
+            /// 2次元配列
+            /// </summary>
+            private T[,] _array;
+            /// <summary>
+            /// 行インデックス
+            /// </summary>
+            private int _rowIndex;
+            #endregion
+
+            #region ■ Properties
+
+            #region - Indexa : インデクサ
+            /// <summary>
+            /// インデクサ
+            /// </summary>
+            /// <param name="colIndex">列インデックス</param>
+            /// <returns>要素</returns>
+            public T this[int colIndex]
+            {
+                get => _array[_rowIndex, colIndex];
+                set => _array[_rowIndex, colIndex] = value;
+            }
+            #endregion
+
+            #region - Length : 長さ
+            /// <summary>
+            /// Length
+            /// </summary>
+            public int Length => _array.GetLength(1);
+            #endregion
+
+            #endregion
+
+            #region ■ Constructor
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="array">2次元配列</param>
+            /// <param name="rowIndex">行インデックス</param>
+            public SquareArrayRow(T[,] array, int rowIndex)
+            {
+                _array = array;
+                _rowIndex = rowIndex;
+            }
+            #endregion
+
+            #region ■ Methods
+
+            #region - GetEnumerator : GetEnumerator()の実装
+            /// <summary>
+            /// GetEnumerator()の実装
+            /// </summary>
+            /// <returns>IEnumerator<T></returns>
+            public IEnumerator<T> GetEnumerator()
+            {
+                for (var i = 0; i < Length; i++)
+                    yield return this[i];
+            }
+            #endregion
+
+            #region - GetEnumerator : GetEnumerator()の実装
+            /// <summary>
+            /// GetEnumerator()の実装
+            /// </summary>
+            /// <returns>IEnumerator</returns>
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
+            #endregion
+
+            #endregion
+        }
+        #endregion
+
         #region ■ Extension Methods
 
         #region - Get : 配列より値を取得する(1)
@@ -189,7 +357,7 @@ namespace hsb.Extensions
         /// <param name="array">配列</param>
         /// <param name="direction">回転方向</param>
         /// <returns>配列</returns>
-        public static T[,] Rotate<T>(this T[,] array, RotateDirection direction = RotateDirection.Right) where T: new()
+        public static T[,] Rotate<T>(this T[,] array, RotateDirection direction = RotateDirection.Right)
         {
             T[,] RightDirection()
             {
